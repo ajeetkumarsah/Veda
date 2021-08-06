759820c5a62ce1b9ff9ee95497ab58f24dfab2ac
Ajeet Kumar Sah <ajeetkumarshaw0367@gmail.com>
2021-08-06 11:33:26 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/veda/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.veda
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,129 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Image',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyImagePicker(title: 'Veda'),
+    );
+  }
+}
+
+class MyImagePicker extends StatefulWidget {
+  MyImagePicker({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyImagePickerState createState() => _MyImagePickerState();
+}
+
+class _MyImagePickerState extends State<MyImagePicker> {
+  PickedFile? _imageFile;
+  final String uploadUrl = 'https://codelime.in/api/remind-app-token';
+  final ImagePicker _picker = ImagePicker();
+
+  Future<String?> uploadImage(filepath, url,context) async {
+    var request = http.MultipartRequest('POST', Uri.parse(url));
+    request.files.add(await http.MultipartFile.fromPath('image', filepath));
+    var res = await request.send();
+    print('${res.statusCode}');
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text("Image upload status: ${res.reasonPhrase}"),
+    ));
+    return res.reasonPhrase;
+  }
+
+  Future<void> retriveLostData() async {
+    final LostData response = await _picker.getLostData();
+    if (response.isEmpty) {
+      return;
+    }
+    if (response.file != null) {
+      setState(() {
+        _imageFile = response.file!;
+      });
+    } else {
+      print('Retrieve error ' + response.exception!.code);
+    }
+  }
+
+  Widget _previewImage() {
+    if (_imageFile != null) {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.file(File(_imageFile!.path)),
+            SizedBox(
+              height: 20,
+            ),
+            RaisedButton(
+              onPressed: () async {
+                var res = await uploadImage(_imageFile!.path, uploadUrl,context);
+                print(res);
+              },
+              child: const Text('Upload'),
+            )
+          ],
+        ),
+      );
+    } else {
+      return const Text(
+        'You have not yet picked an image.',
+        textAlign: TextAlign.center,
+      );
+    }
+  }
+
+  void _pickImage() async {
+    try {
+      final pickedFile = await _picker.getImage(source: ImageSource.gallery);
+      setState(() {
+        _imageFile = pickedFile!;
+      });
+    } catch (e) {
+      print("Image picker error ");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+          child: FutureBuilder<void>(
+        future: retriveLostData(),
+        builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return const Text('Picked an image');
+            case ConnectionState.done:
+              return _previewImage();
+            default:
+              return const Text('Picked an image');
+          }
+        },
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _pickImage,
+        tooltip: 'Pick Image from gallery',
+        child: Icon(Icons.photo_library),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
